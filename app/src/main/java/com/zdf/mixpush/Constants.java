c2039f32835e66181948eb785b9f88b7977fdce5
zhangdanfeng <zhangdanfeng@idongjia.cn>
2017-04-19 15:49:18 +0800

    init

---
+++ app/src/main/java/com/zdf/mixpush/Constants.java
@@ -0,0 +1,12 @@
+package com.zdf.mixpush;
+
+/**
+ * Created by xiaofeng on 2017/4/19.
+ */
+
+public class Constants {
+
+    public static final String UMENG_APP_KEY = "58f70ad3734be428290001e0";
+    public static final String UMENG_MESSAGE_SECRET = "d97c15d4b552ebe95b9f5c7f82e22cda";
+    public static final String UMENG_MASTER_SECRET = "esi6t6cknrf1g3fgcyw3fra6ohkgixaf";
+}
